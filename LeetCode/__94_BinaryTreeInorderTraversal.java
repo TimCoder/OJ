d007fd1681d6f382a60c3fc278e1b74dd4feca98
Tim.Wu <Tim@TimWudeMacBook-Pro.local>
2016-09-29 15:44:56 +0800

    binary tree inorder traversal

---
+++ LeetCode/__94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,29 @@
+package tim.leetcode; 
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class __94_BinaryTreeInorderTraversal {
+    
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode (int x) {
+            val = x;
+        }
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        public List<Integer> inorderTraversal(TreeNode root) {
+            if (root != null) {
+                inorderTraversal(root.left);
+                list.add(root.val);
+                inorderTraversal(root.right); 
+            }
+            return list;
+        }
+
+    }
+    
+}
