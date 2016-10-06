9e0587c82aad8b0420e68c93967f6bffece73223
TimCoder <TimWu.Coder@gmail.com>
2016-10-06 12:52:17 +0800

    hihocoder a + b

---
+++ hihoCoder/__1000.java
@@ -0,0 +1,14 @@
+package tim.hihocoder;
+
+import java.util.Scanner;
+
+public class __1000 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
