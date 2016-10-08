2c6b96da5aa45bdf5c774f1ecd961062a746913d
TimCoder <TimWu.Coder@gmail.com>
2016-10-08 15:34:05 +0800

    The Marshtomp has seen it all before

---
+++ hihoCoder/__1082.java
@@ -0,0 +1,16 @@
+package tim.hihocoder;
+
+import java.util.Scanner;
+
+public class __1082 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String line = in.nextLine();
+            String result = line.replaceAll("[mM][aA][rR][sS][hH][tT][oO][mM][pP]", "fjxmlhx");
+            System.out.println(result);
+        }
+    }
+
+}
