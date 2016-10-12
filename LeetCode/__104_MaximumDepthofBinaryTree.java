e0b8d5553fbf9942ebc7d371b6bf5b58c53795b8
TimCoder <TimWu.Coder@gmail.com>
2016-10-12 15:58:56 +0800

    Maximum Depth of Binary Tree

---
+++ LeetCode/__104_MaximumDepthofBinaryTree.java
@@ -0,0 +1,20 @@
+package tim.leetcode;
+
+public class __104_MaximumDepthofBinaryTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode (int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }  
+        return 1 + (int)Math.max(maxDepth(root.left), maxDepth(root.right)); 
+    }
+
+}
