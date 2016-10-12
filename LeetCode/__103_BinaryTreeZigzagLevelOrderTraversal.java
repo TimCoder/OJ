b1e651b60e2f5012c0c97779f125f96f5554e44d
TimCoder <TimWu.Coder@gmail.com>
2016-10-12 19:00:38 +0800

    Binary Tree Zigzag Level Order Traversal with 2 algorithms

--- LeetCode/__103_BinaryTreeZigzagLevelOrderTraversal.java
+++ LeetCode/__103_BinaryTreeZigzagLevelOrderTraversal.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 public class __103_BinaryTreeZigzagLevelOrderTraversal {
     class TreeNode {
@@ -16,7 +17,58 @@ public class __103_BinaryTreeZigzagLevelOrderTraversal {
         }
     }
 
+
+    //using 2 stack, more space less time than second algorithm.
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        
+        if (root == null) {
+            return result;
+        }
+        
+        Stack<TreeNode> currLevel = new Stack<TreeNode>();
+        Stack<TreeNode> nextLevel = new Stack<TreeNode>();
+        Stack<TreeNode> tmp;
+        
+        currLevel.push(root);
+        boolean normalOrder = true;
+
+        while (!currLevel.isEmpty()) {
+            ArrayList<Integer> currLevelResult = new ArrayList<Integer>();
+
+            while (!currLevel.isEmpty()) {
+                TreeNode node = currLevel.pop();
+                currLevelResult.add(node.val);
+
+                if (normalOrder) {
+                    if (node.left != null) {
+                        nextLevel.push(node.left);
+                    }
+                    if (node.right != null) {
+                        nextLevel.push(node.right);
+                    }
+                } else {
+                    if (node.right != null) {
+                        nextLevel.push(node.right);
+                    }
+                    if (node.left != null) {
+                        nextLevel.push(node.left);
+                    }
+                }
+            }
+
+            result.add(currLevelResult);
+            tmp = currLevel;
+            currLevel = nextLevel;
+            nextLevel = tmp;
+            normalOrder = !normalOrder;
+        }
+
+        return result;
+    }
+
+    //using queue, and reverse list, more time less spqce than first algorithm.
+    public List<List<Integer>> zigzagLevelOrder2(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         if (root == null) {
             return result;
