ee96bbd70b0094d1861bac1c96b5392c8fae2bd4
TimCoder <TimWu.Coder@gmail.com>
2016-10-13 14:03:59 +0800

    Minimum Depth of Binary Tree

--- LeetCode/__111_MinimumDepthofBinaryTree.java
+++ LeetCode/__111_MinimumDepthofBinaryTree.java
@@ -11,23 +11,19 @@ public class __111_MinimumDepthofBinaryTree {
         }
     }
     
-    boolean isRoot = true;
     public int minDepth(TreeNode root) {
-
+        
         if (root == null) {
             return 0;
         }
         
-        if (isRoot) {
-            isRoot = !isRoot;
-            if (root.left == null) {
-                return 1 + minDepth(root.right);
-            }
-            if (root.right == null) {
-                return 1 + minDepth(root.left);
-            }
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
         }
-        
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+     
         return 1 + Math.min(minDepth(root.left), minDepth(root.right));
         
     }
