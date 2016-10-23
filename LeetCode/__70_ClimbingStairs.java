945d2de8904af217abe94068f93241f3baff5b7a
TimCoder <TimWu.Coder@gmail.com>
2016-10-23 20:27:32 +0800

    Climbing Stairs using time O(log N) by matrix

--- LeetCode/__70_ClimbingStairs.java
+++ LeetCode/__70_ClimbingStairs.java
@@ -1,9 +1,83 @@
 package tim.leetcode;
 
+import java.util.Scanner;
+
 public class __70_ClimbingStairs {
-
-
     
+    /*
+     * time O(n)
+     */
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        System.out.println(climbStairs(N));
+        
+    }
+
+    public static class Matrix2x2 {
+
+        int m00;
+        int m01;
+        int m10;
+        int m11;
+
+        Matrix2x2(int x00, int x01, int x10, int x11) {
+            this.m00 = x00;
+            this.m01 = x01;
+            this.m10 = x10;
+            this.m11 = x11;
+        }
+
+    }
+
+    public static int climbStairs(int n) {
+
+        if (n <= 1) {
+            return 1;
+        }
+
+        Matrix2x2 m = matrixPower(n);
+
+        return m.m00;
+
+    }
+
+    public static Matrix2x2 matrixPower(int n) { 
+
+        Matrix2x2 m = new Matrix2x2(1, 1, 1, 0);
+
+        if (n == 1) {
+            return m;
+        } else if (n % 2 == 0) {
+            m = matrixPower(n/2);
+            m = multiplyMatrix(m, m);
+        } else if (n % 2 == 1) {
+            m = matrixPower((n-1)/2);
+            m = multiplyMatrix(m, m);
+            m = multiplyMatrix(m, new Matrix2x2(1, 1, 1, 0));
+        }
+
+        return m;
+
+    }
+
+    public static Matrix2x2 multiplyMatrix(Matrix2x2 m1, Matrix2x2 m2) {
+
+        Matrix2x2 m = new Matrix2x2(1, 1, 1, 0);
+        m.m00 = m1.m00 * m2.m00 + m1.m01 * m2.m10;
+        m.m01 = m1.m00 * m2.m01 + m1.m01 * m2.m11;
+        m.m10 = m1.m10 * m2.m00 + m1.m11 * m2.m10;
+        m.m11 = m1.m10 * m2.m01 + m1.m11 * m2.m11;
+
+        return m;
+
+    }
+    
+    /*
+     * time O(n)
+     */
+    /*
     public int climbStairs(int n) {
 
         if (n <= 1) {
@@ -21,10 +95,8 @@ public class __70_ClimbingStairs {
 
         return c;
 
-    }   
-
-
-
+    }  
+    */ 
 
     /*
      * TLE, time O(2^n)
