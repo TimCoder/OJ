a68fda0aab6e7ec7ac6f7ce6c468c5d1f93be54c
TimCoder <TimWu.Coder@gmail.com>
2016-11-01 19:58:45 +0800

    Unique Paths

---
+++ LeetCode/__62_UniquePaths.java
@@ -0,0 +1,38 @@
+package tim.leetcode;
+
+public class __62_UniquePaths {
+
+    /*
+     * time O(m * n), space: O(min(m,n)).
+     */
+    public int uniquePaths(int m, int n) {
+        
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        
+        if (m == 2) {
+            return n;
+        }
+            
+        if (n == 2) {
+            return m;
+        } 
+        //cur line
+        int[] cur = new int[n];
+
+        for (int j = 0; j < n; ++j) { 
+            cur[j] = 1;
+        }   
+
+        for (int i = 1; i < m; ++i) { 
+            for (int j = 1; j < n; ++j) {
+                cur[j] = cur[j - 1] + cur[j];
+            }   
+        }   
+
+        return cur[n - 1]; 
+
+    }
+    
+}
