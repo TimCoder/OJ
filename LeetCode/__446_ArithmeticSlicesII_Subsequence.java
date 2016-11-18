22d4ebc6dc979916643203719060dc492c417272
TimCoder <TimWu.Coder@gmail.com>
2016-11-18 09:47:03 +0800

    Arithmetic Slices II

---
+++ LeetCode/__446_ArithmeticSlicesII_Subsequence.java
@@ -0,0 +1,35 @@
+package tim.leetcode;
+
+public class __446_ArithmeticSlicesII_Subsequence {
+
+    /*
+     * Time : O(N ^ 2), Space : O(N ^ 2).
+     */
+    public int numberOfArithmeticSlices(int[] A) {
+
+        int res = 0;
+        int N = A.length;
+        HashMap<Integer, Integer>[] map = new HashMap[N];
+
+        for (int i = 0; i < N; ++i) { 
+            map[i] = new HashMap(i);
+            for (int j = 0; j < i; ++j) {
+                //using long avoid overflow Integer range.
+                long diff = (long)A[i] - A[j];
+                if (diff < Integer.MIN_VALUE || diff > Integer.MAX_VALUE) {
+                    continue;
+                }
+
+                int d = (int)diff;
+                int sumI = map[i].getOrDefault(d, 0);
+                int sumJ = map[j].getOrDefault(d, 0);
+                res += sumJ;
+                map[i].put(d, sumI + sumJ + 1);
+            }
+        }
+
+        return res;
+
+    }
+
+}
