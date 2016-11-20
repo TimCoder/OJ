a6954b6c4e5938fb8a758819616b38a367625538
TimCoder <TimWu.Coder@gmail.com>
2016-11-20 11:06:17 +0800

    different bits method

--- LeetCode/__342_PowerofFour.java
+++ LeetCode/__342_PowerofFour.java
@@ -10,6 +10,7 @@ public class __342_PowerofFour {
         //0x55555555 is to get rid of those power of 2 but not power of 4
         //so that the single 1 bit always appears at the odd position 
         return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
+        //return num > 0 && (num&(num-1)) == 0 && (num & 0xaaaaaaaa) == 0;
     }
 
     /*
