5f3123c91c4f4f78aa9ed6838a3d077b1431d1eb
TimCoder <TimWu.Coder@gmail.com>
2016-11-20 10:57:27 +0800

    Power of Four

---
+++ LeetCode/__342_PowerofFour.java
@@ -0,0 +1,36 @@
+package tim.leetcode;
+
+public class __342_PowerofFour {
+
+    /*
+     * Time : O(1).
+     * using time : 2 ms.
+     */
+    public boolean isPowerOfFour(int num) {
+        //0x55555555 is to get rid of those power of 2 but not power of 4
+        //so that the single 1 bit always appears at the odd position 
+        return num > 0 && (num&(num-1)) == 0 && (num & 0x55555555) != 0;
+    }
+
+    /*
+     * Time : O(1).
+     * using time : 2 ms.
+     */
+    /*
+    public boolean isPowerOfFour(int num) {
+        return Integer.toString(num, 4).matches("^10*$");
+    }
+    */
+
+    /*
+     * WA. Base must be prime when using this method.
+     * Time : O(1).
+     * using time : 2 ms.
+     */
+    /*
+    public boolean isPowerOfFour(int num) {
+        return n > 0 && 1073741824 % num == 0;
+    }
+    */
+
+}
