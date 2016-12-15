cd393a58881899c37d4516485f57b8b051e42fa4
Tim.Wu <timwu.coder@gmail.com>
2016-12-15 16:29:17 +0800

    Remove Element

---
+++ LeetCode/__27_RemoveElement.java
@@ -0,0 +1,25 @@
+package tim.leetcode;
+
+public class __27_ReomveElement {
+
+    public int removeElement(int[] nums, int val) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int i = 0, n = nums.length;
+        while (i < n) {
+            if (nums[i] != val) {
+                ++i;
+            } else {
+                nums[i] = nums[n - 1];
+                --n;
+            }
+        }
+
+        return n;
+
+    }
+
+}
