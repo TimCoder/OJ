bd5b32d4866de8d1efdaff2d6821634f960dff19
Tim.Wu <Tim@TimWudeMacBook-Pro.local>
2016-12-15 14:33:45 +0800

    Two Sum

---
+++ LeetCode/__1_TwoSum.java
@@ -0,0 +1,28 @@
+package tim.leetcode;
+
+public class __1_TwoSum {
+
+    /*
+     *  [0,4,3,0]
+     *  0
+     */ 
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] res = new int[2];
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; ++i) { 
+            int complement = target - nums[i];
+            if (map.contains(complement)) {
+                res[0] = map.get(complement);
+                res[1] = i;
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+
+        return res;
+                
+    }
+
+}
