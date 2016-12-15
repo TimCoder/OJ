d9a1c3e0562f633a963eb6eb1932c7322b66eeb6
Tim.Wu <Tim@TimWudeMacBook-Pro.local>
2016-12-15 14:50:03 +0800

    Third Maximum Number

---
+++ LeetCode/__414_ThirdMaximumNumber.java
@@ -0,0 +1,57 @@
+package tim.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class __414_ThirdMaximumNumber {
+
+    public int thirdMax(int[] nums) {
+
+        if (nums == null || nums.length == 0) { 
+            return Integer.MIN_VALUE;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, nums[0]);
+        for (int i = 1; i < nums.length; ++i) {
+            if (!map.containsKey(1)) { 
+                if (nums[i] > map.get(0)) {
+                    map.put(1, map.get(0));
+                    map.put(0, nums[i]); 
+                } else if (nums[i] < map.get(0)) {
+                    map.put(1, nums[i]);
+                }
+            } else if (!map.containsKey(2)) {
+                if (nums[i] > map.get(0)) {
+                    map.put(2, map.get(1));
+                    map.put(1, map.get(0));
+                    map.put(0, nums[i]);
+                } else if (nums[i] < map.get(0) && nums[i] > map.get(1)) { 
+                    map.put(2, map.get(1));
+                    map.put(1, nums[i]);
+                } else if (nums[i] < map.get(1)) {
+                    map.put(2, nums[i]);
+                }
+            } else {
+                if (nums[i] > map.get(0)) {
+                    map.put(2, map.get(1));
+                    map.put(1, map.get(0));
+                    map.put(0, nums[i]);
+                } else if (nums[i] < map.get(0) && nums[i] > map.get(1)) {
+                    map.put(2, map.get(1));
+                    map.put(1, nums[i]);
+                } else if (nums[i] < map.get(1) && nums[i] > map.get(2)) {
+                    map.put(2, nums[i]);
+                }
+            }
+        }
+
+        if (!map.containsKey(2)) {
+            return map.get(0); 
+        } else {
+            return map.get(2);
+        }
+
+    }
+
+}
