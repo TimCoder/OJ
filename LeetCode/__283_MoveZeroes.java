23e37baf065f702189c714408a8eeb0e85cbc451
Tim.Wu <timwu.coder@gmail.com>
2016-12-16 15:23:29 +0800

    Move Zeroes

---
+++ LeetCode/__283_MoveZeroes.java
@@ -0,0 +1,22 @@
+package tim.leetcode;
+
+public class __283_MoveZeros {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int j = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i]; 
+                ++j;
+            }
+        }
+
+        while (j < nums.length) {
+            nums[j] = 0;
+            ++j;
+        }
+    }
+}
