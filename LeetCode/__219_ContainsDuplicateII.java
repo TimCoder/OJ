8262eeff2444d59fb900c712bdd238a089ae3f47
Tim.Wu <timwu.coder@gmail.com>
2016-12-16 14:36:25 +0800

    Contains Duplicate II

---
+++ LeetCode/__219_ContainsDuplicateII.java
@@ -0,0 +1,21 @@
+package tim.leetcode;
+
+public class __219_ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length < 2 || k < 0) {
+            return false;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(nums[i])) {
+                int diff = i - map.get(nums[i]);
+                if (diff <= k) {
+                    return true;
+                }
+            } 
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
