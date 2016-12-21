1781157c5df615b272414c4fe7aafb131a3e7601
Tim.Wu <timwu.coder@gmail.com>
2016-12-21 13:50:37 +0800

    Time: O(log N)

--- LeetCode/__35_SearchInsertPosition.java
+++ LeetCode/__35_SearchInsertPosition.java
@@ -1,6 +1,29 @@
 package tim.leetcode;
 
 public class __35_SearchInsertPosition {
+    /*
+     * Time : O(log N).
+     */
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + ((r - l) >> 1);
+            if (nums[m] == target) { 
+                return m;
+            } else if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return l;
+    }
+
+    /*
+     * Time : O(N).
+     */
+    /*
     public int searchInsert(int[] nums, int target) {
         for (int i = 0; i < nums.length; ++i) {
             if (nums[i] >= target) {
@@ -10,4 +33,5 @@ public class __35_SearchInsertPosition {
 
         return nums.length;
     }
+    */
 }
