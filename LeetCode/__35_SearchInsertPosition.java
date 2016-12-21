a17779d8e7d55d3b513b9f391c16963be92d3fc3
Tim.Wu <timwu.coder@gmail.com>
2016-12-21 13:26:30 +0800

    Search Insert Position

---
+++ LeetCode/__35_SearchInsertPosition.java
@@ -0,0 +1,13 @@
+package tim.leetcode;
+
+public class __35_SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+}
