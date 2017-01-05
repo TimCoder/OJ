71402b9773b014a93d89e6a2dcfa4678a15a07db
Tim.Wu <timwu.coder@gmail.com>
2017-01-05 13:20:29 +0800

    Permutations II

---
+++ LeetCode/__47_PermutationsII.java
@@ -0,0 +1,32 @@
+package tim.leetcode;
+
+public class __47_PermutationsII {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        List<Integer> tmp = new ArrayList<>();
+        backtrack(res, tmp, used, nums);
+        return res;
+    }
+
+    public void backtrack(List<List<Integer>> res, List<Integer> tmp, boolean[] used, int[] nums) {
+        if (tmp.size() == nums.length) {
+            res.add(new ArrayList<>(tmp));
+        } else {
+            for (int i = 0; i < nums.length; ++i) {
+                if (used[i]) {
+                    continue;
+                }
+                if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                    continue;
+                }
+                used[i] = true;
+                tmp.add(nums[i]);
+                backtrack(res, tmp, used, nums);
+                used[i] = false;
+                tmp.remove(tmp.size() - 1);
+            }
+        }
+    }
+}
