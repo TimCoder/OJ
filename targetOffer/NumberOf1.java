263d64b8fff46f649e0b02da4a9730c207615af2
Tim.Wu <timwu.coder@gmail.com>
2017-03-16 14:09:13 +0800

    Number Of 1

---
+++ targetOffer/NumberOf1.java
@@ -0,0 +1,12 @@
+package tim.targetOffer;
+
+public class NumberOf1 {
+    public int NumberOf1 (int n) {
+        int res = 0;
+        while (n != 0) { 
+            n &= n - 1;
+            ++res;
+        }
+        return res;
+    }
+}
